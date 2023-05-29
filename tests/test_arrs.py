091a8eb325a42dbba92001b9ae403a6862c7d654
artemm <artem.m2013@gmail.com>
2023-05-30 01:38:01 +0300

    get 100% tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,9 +6,21 @@ class TestArrs(unittest.TestCase):
 
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(arrs.get([1, 2, 3], -5, "test"), "test")
         with self.assertRaises(IndexError):
-                arrs.get([], 0, "test")
+            arrs.get([], 0, "test")
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1), [3])
+
+    def test_slice_with_start_greater_than_length(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], 5), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], 10), [])
+
+    def test_slice_with_negative_start(self):
+        self.assertEqual(arrs.my_slice([1, 2, 3], -2), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -4), [1, 2, 3])
+
